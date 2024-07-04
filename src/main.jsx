ce0baac6bb486205dbd4c40d372f31928eaf7e95
hussam633hasan <husamhasan633@gmail.com>
2024-07-04 13:17:00 +0300

    up

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Test-for-demo/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
